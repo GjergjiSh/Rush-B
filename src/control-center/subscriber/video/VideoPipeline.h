cc55aac6a1f175f70384fb15587bc723af1bde55
GjergjiSh <44-gjshk583@users.noreply.gitlab.ias>
2021-07-09 21:06:11 +0200

    Added video subscriber pipeline

---
+++ src/control-center/subscriber/video/VideoPipeline.h
@@ -0,0 +1,48 @@
+#ifndef VIDEOPIPELINE_H
+#define VIDEOPIPELINE_H
+
+#include <gst/gst.h>
+#include <iostream>
+#include <string>
+#include <thread>
+
+/**************************************    Pipeline    *********************************
+
+    gst-launch-1.0 -v udpsrc port=xxxx ! \
+   "application/x-rtp, media=video, clock-rate=90000, encoding-name=H264, payload=96" ! \
+    rtph264depay ! \
+    decodebin ! \
+    videoconvert ! \
+    queue ! \
+    autovideosink
+
+ ****************************************************************************************/
+
+typedef struct
+{
+    GstElement* pipe;
+    GstElement* udpsrc;
+    GstElement* filter;
+    GstElement* rtph264depay;
+    GstElement* decodebin;
+    GstElement* videoconvert;
+    GstElement* queue;
+    GstElement* autovideosink;
+} tVideoPipeline;
+
+class VideoSubscriber {
+
+public:
+    VideoSubscriber();
+    ~VideoSubscriber();
+    int32_t Construct_Pipeline(const char* name, tVideoPipeline* const pipeline);
+    int32_t Destroy_Pipeline();
+    int32_t Set_Pipeline_State_Playing();
+    void Start_Gloop();
+
+    int32_t port;
+    tVideoPipeline pipeline = { 0 };
+    GMainLoop* loop;
+};
+
+#endif /* VIDEOPIPELINE_H */
