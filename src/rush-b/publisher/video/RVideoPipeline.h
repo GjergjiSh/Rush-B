b53ca91ea7718b5c07b8b14853ed9ad63e23ba3b
GjergjiSh <44-gjshk583@users.noreply.gitlab.ias>
2021-07-11 21:49:22 +0200

    Added RSubscriber

---
+++ src/rush-b/publisher/video/RVideoPipeline.h
@@ -0,0 +1,53 @@
+#ifndef VIDEOPIPELINE_H
+#define VIDEOPIPELINE_H
+
+#include <gst/gst.h>
+#include <iostream>
+#include <string.h>
+#include <unordered_map>
+
+typedef struct
+{
+    int tune;
+    int speed_preset;
+    int bitrate;
+} tX264Param;
+
+typedef struct
+{
+    std::string host;
+    int port;
+} tSINKParam;
+
+typedef struct
+{
+    std::string camera;
+    tX264Param x264_param;
+    tSINKParam sink_param;
+} tVideoPipelineParam;
+
+typedef struct
+{
+    GstElement* pipe;
+    GstElement* v4l2src;
+    GstElement* videoconvert;
+    GstElement* x264enc;
+    GstElement* rtph264pay;
+    GstElement* udpsink;
+    tVideoPipelineParam parameters;
+} tVideoPipeline;
+
+class VideoPublisher {
+
+public:
+    VideoPublisher();
+    ~VideoPublisher();
+    int32_t Construct_Pipeline(const char* name, tVideoPipeline* const pipeline);
+    int32_t Destroy_Pipeline();
+    int32_t Set_Pipeline_State_Playing();
+
+    tVideoPipeline pipeline;
+    GMainLoop* loop;
+};
+
+#endif /* VIDEOPIPELINE_H */
