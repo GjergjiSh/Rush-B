815c5fcb5ccbb34c44ef5b7afab6ffb33740aa00
GjergjiSh <44-gjshk583@users.noreply.gitlab.ias>
2021-07-11 21:48:33 +0200

    Renamed sub and pub components to avoid conflicts with control center files

---
+++ src/rush-b/subscriber/RSubscriber.h
@@ -0,0 +1,25 @@
+#ifndef R_SUBSCRIBER_H
+#define R_SUBSCRIBER_H
+
+#include <iostream>
+#include <zmq.hpp>
+#include "PBDriverWish.pb.h"
+
+class Subscriber {
+
+public:
+    Subscriber();
+    ~Subscriber();
+
+    int32_t ZMQ_Init_Connection();
+    int32_t ZMQ_Deinit_Connection();
+    int32_t ZMQ_Receive();
+
+    std::string zqm_sub_transport;
+
+protected:
+    zmq::context_t zmq_sub_context;
+    zmq::socket_t zmq_sub_socket;
+};
+
+#endif //R_SUBSCRIBER_H
