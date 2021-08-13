3cc5f53cfa78248bb4dd2ddcbdd64c058c3b29a0
GjergjiSh <44-gjshk583@users.noreply.gitlab.ias>
2021-08-13 21:42:30 +0200

    added gesture controller

--- src/py-modules/HandDetector.py
+++ src/py-modules/HandDetector.py
@@ -1,5 +1,6 @@
 import cv2
 import mediapipe as mp
+import numpy as np
 import time
 
 
@@ -11,38 +12,67 @@ class HandDetector():
         self.track_confdence = track_confdence
 
         self.mp_hands = mp.solutions.hands
-        self.hands = self.mp_hands.Hands( self.mode, self.max_hands, self.detection_confidence, self.track_confdence)
+        self.hands = self.mp_hands.Hands(
+            self.mode, self.max_hands, self.detection_confidence, self.track_confdence)
         self.mp_draw = mp.solutions.drawing_utils
 
-
-    def find_hands(self, img, draw=True ):
+    def get_results(self, img, draw=False):
         img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         self.results = self.hands.process(img_rgb)
 
-        if self.results.multi_hand_landmarks:
-            for hand_lms in self.results.multi_hand_landmarks:
-                if draw:
-                    self.mp_draw.draw_landmarks(img, hand_lms, self.mp_hands.HAND_CONNECTIONS)
+        if draw:
+            if (self.results.multi_hand_landmarks):
+                for hand_lms in self.results.multi_hand_landmarks:
+                    self.mp_draw.draw_landmarks(
+                        img, hand_lms, self.mp_hands.HAND_CONNECTIONS)
 
         return img
 
-    def find_hand_position(self, img, hand_nr=0, draw=True):
-        landmark_list = []
-
+    def draw_finger_angles(self, image, joint_list):
+        # Loop through hands
         if self.results.multi_hand_landmarks:
-            hand = self.results.multi_hand_landmarks[hand_nr]
+            for hand in self.results.multi_hand_landmarks:
+                #Loop through joint sets
+                for joint in joint_list:
+                    # First coord
+                    a = np.array([hand.landmark[joint[0]].x,
+                                  hand.landmark[joint[0]].y])
+                    # Second coord
+                    b = np.array([hand.landmark[joint[1]].x,
+                                  hand.landmark[joint[1]].y])
+                    # Third coord
+                    c = np.array([hand.landmark[joint[2]].x,
+                                  hand.landmark[joint[2]].y])
 
-            for id, lm in enumerate(hand.landmark):
-                h, w, c = img.shape
-                cx, cy = int(lm.x*w), int(lm.y*h)
-                landmark_list.append([id, cx, cy])
+                    radians = np.arctan2(
+                        c[1] - b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
+                    angle = np.abs(radians*180.0/np.pi)
 
-                if draw:
-                    cv2.circle(img, (cx,cy), 8, (0,0,255), cv2.FILLED)
+                    if angle > 180.0:
+                        angle = 360-angle
+
+                    cv2.putText(image, str(round(angle, 2)), tuple(np.multiply(b, [640, 480]).astype(int)),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+
+        return image
+
+    def find_hand_position(self, img, handedness):
+        landmark_list = []
+        if self.results.multi_hand_landmarks:
+            for num, hand in enumerate(self.results.multi_hand_landmarks):
+                for idx, classification in enumerate(self.results.multi_handedness):
+                    if (classification.classification[0].index == handedness) & (num == handedness):
+                        for id, lm in enumerate(hand.landmark):
+                            h, w, c = img.shape
+                            cx, cy = int(lm.x*w), int(lm.y*h)
+                            landmark_list.append([id, cx, cy])
+
         return landmark_list
 
+
 def main():
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(4)
+    joint_list = [[8, 7, 6], [12, 11, 10], [16, 15, 14], [20, 19, 18]]
     pTime = 0
     cTime = 0
 
@@ -54,8 +84,8 @@ def main():
         fps = 1/(cTime - pTime)
         pTime = cTime
 
-        img = detector.find_hands(img)
-        landmark_list = detector.find_hand_position(img)
+        img = detector.get_results(img)
+        img = detector.draw_finger_angles(img, joint_list)
 
         cv2.putText(img, str(int(fps)), (10, 70),
                     cv2.FONT_HERSHEY_PLAIN, 3, (0, 0, 255), 2)
--- src/py-modules/ZMQPipeline.py
+++ src/py-modules/ZMQPipeline.py
@@ -10,8 +10,9 @@ class DWPublisher():
         self.dw_pub_socket = self.zmq_context.socket(zmq.PUSH)
         self.dw_pub_socket.setsockopt(zmq.LINGER, 0)
         self.dw_pub_socket.bind("ipc://{}".format(interface_transport))
+        print('[I] [PyDW Publisher] ZMQ Connection initialized')
 
-    def DriverWish(self, top_servo=0, left_servo=90, right_servo=90):
+    def create_driver_wish(self, top_servo=0, left_servo=90, right_servo=90):
         driver_wish = dw.PBDriverWish()
         driver_wish.top_servo = top_servo
         driver_wish.left_servo = left_servo
@@ -22,6 +23,12 @@ class DWPublisher():
     def publish(self, driver_wish):
         message = driver_wish.SerializeToString()
         self.dw_pub_socket.send(message)
+
+    def kill(self):
+        self.dw_pub_socket.close()
+        self.zmq_context.destroy()
+        print('[I] [PyDW Publisher] ZMQ Connection deinitialized')
+
 
 
 if __name__ == '__main__':
